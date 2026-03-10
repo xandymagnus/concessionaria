a1b6a6632b48a9faf144a5340a96d6041cbe8f81
xandymagnus <xandy.magno325@gmail.com>
2026-03-10 10:23:30 -0300

    implementando saida a classe

--- veiculos/moto.py
+++ veiculos/moto.py
@@ -7,4 +7,5 @@ class Moto(Concessionaria):
         self._estilo = estilo
         self._bagageiro = False
         
-        
+    def __str__(self):
+        return super().__str__() + f' | Estilo: {self._estilo} | Bagageiro: {self._bagageiro}'
