38ec5f0f391a51f2b236ee0e5ee7930a4725cff9
xandymagnus <xandy.magno325@gmail.com>
2026-03-10 10:22:57 -0300

    testando nova classe filha moto

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from concessionaria import Concessionaria
 from veiculos.carro import Carro
+from veiculos.moto import Moto
 
-
-veiculo1 = Concessionaria('bmw', 'm3', '2005')
+veiculo1 = Carro('bmw', 'm3', '2005', '4', '6')
 veiculo2 = Concessionaria('mercedes', 'g63', '2010')
 veiculo3 = Concessionaria('audi', 'r8', '2013')
 veiculo3.alterar_estado()
 
+novaMoto = Moto('bmw', 'g3', '2021', 'Esportiva')
+
 def main():
     Concessionaria.lista_veiculo()
+    print(novaMoto)
         
 if __name__ == '__main__':
     main()
