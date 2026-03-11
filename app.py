0e0cd9e509509ba93db76d782a446684c45cc3ac
xandymagnus <xandy.magno325@gmail.com>
2026-03-11 19:33:23 -0300

    Criando função para testar a  adição automática de atributos à instância através de inputs

--- app.py
+++ app.py
@@ -2,16 +2,28 @@ from concessionaria import Concessionaria
 from veiculos.carro import Carro
 from veiculos.moto import Moto
 
-veiculo1 = Carro('bmw', 'm3', '2005', '4', '6')
-veiculo2 = Concessionaria('mercedes', 'g63', '2010')
-veiculo3 = Concessionaria('audi', 'r8', '2013')
-veiculo3.alterar_estado()
+def atributos_carro():
+    print('Adicione os atributos:\n')
+     
+    marca = input('Marca: ')
+    modelo = input('Modelo: ')
+    ano = input('Ano: ')
+    portas = input('Portas: ')
+    assentos = input('Assentos: ')
+    
+    return marca, modelo, ano, portas, assentos
 
+def atributos_moto():
+    pass
+
+veiculo1 = Carro(*atributos_carro())
+veiculo2 = Carro(*atributos_carro())
+veiculo3 = Carro(*atributos_carro())
 novaMoto = Moto('bmw', 'g3', '2021', 'Esportiva')
+
 
 def main():
     Concessionaria.lista_veiculo()
-    print(novaMoto)
-        
+    print('2' == 2)
 if __name__ == '__main__':
     main()
