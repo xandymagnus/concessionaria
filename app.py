c0a33fd5cbea09ad6adfd9083d1d8af06ae2c5da
xandymagnus <xandy.magno325@gmail.com>
2026-03-12 10:12:03 -0300

    adicionando funções que inputs para fins de preencher atributos da classe

--- app.py
+++ app.py
@@ -2,33 +2,54 @@ from concessionaria import Concessionaria
 from veiculos.carro import Carro
 from veiculos.moto import Moto
 
-def atributos_carro():
-    print('Adicione os atributos:\n')
-     
+def atributos_concessionaria():
+    print('\nAdicione os atributos:\n')
+    
     marca = input('Marca: ')
     modelo = input('Modelo: ')
     ano = input('Ano: ')
+    
+    return marca, modelo, ano
+
+def atributos_carro():
+    print('\nCriando carro...')
+    
+    marca, modelo, ano = atributos_concessionaria()
+
     portas = input('Portas: ')
     assentos = input('Assentos: ')
-    print('\n')
+    automatico = input('Automático: ')
     
-    return marca, modelo, ano, portas, assentos
+    return marca, modelo, ano, portas, assentos, automatico
 
 def atributos_moto():
-    pass
+    print('\nCriando moto...')
+    
+    marca, modelo, ano = atributos_concessionaria()
+    
+    estilo = input('Estilo: ')
+    cor = input('Cor: ')
+    
+    return marca, modelo, ano, estilo, cor
 
-veiculo1 = Carro(*atributos_carro())
-veiculo1.alterar_estado()
-carro1 = Carro(*atributos_carro())
-carro2 = Carro(*atributos_carro())
+moto1 = Moto(*atributos_moto())
+moto1.alterar_estado()
+
+moto2 = Moto(*atributos_moto())
+moto3 = Moto(*atributos_moto())
 veiculo2 = Concessionaria('bmw', '312', '2019')
 
-novaMoto = Moto('bmw', 'g3', '2021', 'Esportiva')
+carro1 = Carro(*atributos_carro())
+
+novaMoto = Moto('bmw', 'g3', '2021', 'Esportiva', 'Preta')
 
 
 def main():
     Carro.lista_carros()
-    Carro.lista_veiculo()
+    print('\n')
+    Moto.lista_motos()
+    print('\n')
+    Concessionaria.lista_veiculo()
     
 if __name__ == '__main__':
     main()
