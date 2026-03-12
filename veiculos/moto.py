26f33f4648ae6a8f1cfe6a15b4187f103a4229a0
xandymagnus <xandy.magno325@gmail.com>
2026-03-12 10:14:46 -0300

    criando array que vaireceber lista de motos; fatorando melhor os atributos; criando método lista_motos

--- veiculos/moto.py
+++ veiculos/moto.py
@@ -1,11 +1,23 @@
 from concessionaria import Concessionaria
 
 class Moto(Concessionaria):
-    def __init__(self, marca, modelo, ano, estilo):
+    motos = []
+    
+    def __init__(self, marca, modelo, ano, estilo, cor):
         super().__init__(marca, modelo, ano)
         
-        self._estilo = estilo
+        self._estilo = str(estilo.upper())
+        self._cor = str(cor.upper())
         self._bagageiro = False
+        Moto.motos.append(self)
         
     def __str__(self):
         return super().__str__() + f' | Estilo: {self._estilo} | Bagageiro: {self._bagageiro}'
+    
+    @classmethod
+    def lista_motos(cls):
+        print(f'{'Marca'.ljust(25)} | {'Modelo'.ljust(25)} | {'Ano'.ljust(25)} | {'Estilo'.ljust(25)} | {'Cor'.ljust(25)} | {'Bagageiro'.ljust(25)} | {'Venda'}\n {'-' * 190}')
+        
+        for moto in cls.motos:
+            if moto._venda == True:
+                print(f'{moto._marca.ljust(25)} | {moto._modelo.ljust(25)} | {moto._ano.ljust(25)} | {moto._estilo.ljust(25)} | {moto._cor.ljust(25)} | {'Sim'.ljust(25) if moto._bagageiro == True else 'Nâo'.ljust(25)} | {moto._venda}')
